34018f64d12309f9426b6aae610baaadcb323ad7
Jonathan Degardin <jdegardin@sio.jjr>
2019-07-03 10:41:01 +0200

    Résolution du conflit

--- Personnage.cs
+++ Personnage.cs
@@ -1,4 +1,9 @@
 class Personnage {
-public void Frapper(Personnage personnage,int force){}
-public void Deplacer(Direction direction) {}
+public void Frapper(Personnage personnage,int force){
+}
+public void Deplacer(Direction direction) {
+}
+public Lieu Regarder(Direction direction,int distance){
+}
+
 }
